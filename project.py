9397bb938e598f907ceb119d3ba22f6939e30212
Himanshu Choudhary <hchoudhary525@gmail.com>
2024-01-02 21:57:45 +0530

    Removed duplicates files and setting up setup files

--- project.py
+++
@@ -1,7 +0,0 @@
-import numpy as np 
-import pandas as pd 
-import matplotlib.pyplot as plt 
-import seaborn as sns 
-
-df=pd.read_csv('C:/Users/hchou/OneDrive/Desktop/Data Science/Projects/DATA/Ames_Housing_Data.csv')
-df.head()
